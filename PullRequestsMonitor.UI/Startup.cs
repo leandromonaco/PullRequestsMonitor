36bd5635e47525649ee8e464617c89b1a458b079
leandromonaco <leandro.monaco@gmail.com>
2020-11-27 11:17:20 +1300

    Replaced Table with MudBlazor Component

--- PullRequestsMonitor.UI/Startup.cs
+++ PullRequestsMonitor.UI/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace PullRequestsMonitor.UI
 {
@@ -20,6 +22,9 @@ namespace PullRequestsMonitor.UI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMudBlazorDialog();
+            services.AddMudBlazorSnackbar();
+            services.AddMudBlazorResizeListener();
             services.AddRazorPages();
             services.AddServerSideBlazor();
         }
